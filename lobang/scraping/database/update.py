0a4df9863cac736e69882a572571c6c86439bc06
Jovian Heok <jovianheok@gmail.com>
2026-06-24 15:25:19 +0800

    docs: clarify purpose of expired deals update function

--- lobang/scraping/database/update.py
+++ lobang/scraping/database/update.py
@@ -1,11 +1,14 @@
 """
-Purpose: Mark status of expired deals to 'expired' to tell frontend not to display
+Purpose: Mark status of expired deals if expiry_date < now() to tell frontend not to display
 """
 
 from .connection import get_conn
 from .queries import UPDATE_EXPIRED_DEALS
 
 def update_expired_deals() -> int:
+    """
+    Purpose: Mark status of expired deals if expiry_date < now() to tell frontend not to display
+    """
     conn = get_conn()
     try:
         with conn:
